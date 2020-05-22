a5a4aef5df19f4e116d85eeff25c0996fa78f8da
kojirock <kojirock5260@gmail.com>
2020-05-23 07:49:53 +0900

    テストでtodoリスト作ってみる

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,45 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        example01
-      </h1>
-      <h2 class="subtitle">
-        My world-class Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div class="bg-blue-100">
+    <h1 class="text-2xl">Todo List</h1>
+
+    <div class="flex">
+      <div class="flex-1 bg-gray-100 m-2">
+        <p class="text-xl">todo</p>
+        <div class="z-10 flex-auto bg-white m-2 p-3 shadow">
+          card1
+        </div>
+      </div>
+
+      <div class="flex-1 bg-gray-100 m-2">
+        <p class="text-xl">doing</p>
+        <div class="z-10 flex-auto bg-white m-2 p-3 shadow">
+          card1
+        </div>
+        <div class="z-10 flex-auto bg-white m-2 p-3 shadow">
+          card2
+        </div>
+        <div class="z-10 flex-auto bg-white m-2 p-3 shadow">
+          card3
+        </div>
+      </div>
+
+      <div class="flex-1 bg-gray-100 m-2">
+        <p class="text-xl">done</p>
+        <div class="z-10 flex-auto bg-white m-2 p-3 shadow">
+          card1
+        </div>
+        <div class="z-10 flex-auto bg-white m-2 p-3 shadow">
+          card2
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
-  components: {
-    Logo
-  }
+  components: {}
 }
 </script>
 
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-  @apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
+<style></style>
